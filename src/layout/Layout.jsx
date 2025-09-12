89ff834c03e99804654a5f51656da9c1f769187d
sultan93291 <abibdipto@gmail.com>
2025-09-12 12:50:23 +0600

    feat:scroll restoration issue fixed

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../shared/Navbar";
 import Footer from "../shared/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 
 const Layout = () => {
+  useEffect(() => {
+    const prev = window.history.scrollRestoration;
+    window.history.scrollRestoration = "manual";
+    return () => {
+      window.history.scrollRestoration = prev || "auto";
+    };
+  }, []);
+
   return (
     <>
       <Navbar />
       <Outlet />
       <Footer />
+      <ScrollRestoration getKey={location => location.pathname} />
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { RouterProvider, ScrollRestoration } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
 
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router}>
-    <ScrollRestoration />
   </RouterProvider>
 );
